56c52e4545e64c93fd87bb5172a8f5b45db4e75f
Ujjwal-77 <ujjwalprasad19@gmail.com>
2025-04-08 07:17:23 +0530

    "[Ujjwal] Add. Annotation for Logging Method Execution Time"

---
+++ annotations/intermediateLevelProblems/loggingExecutionTime/Main.java
@@ -0,0 +1,24 @@
+package intermediateLevelProblems.loggingExecutionTime;
+
+import java.lang.reflect.Method;
+
+public class Main {
+    public static void main(String[] args) {
+        TaskManager manager= new TaskManager();
+
+        try{
+            // Get the methods of TaskManager class
+            Method task1 = manager.getClass().getMethod("performTask1");
+            Method task2 = manager.getClass().getMethod("performTask2");
+            Method normalTask = manager.getClass().getMethod("performNormalTask");
+
+            // Log execution times for the methods
+            MethodExecutionLogger.LogExecutionTime(task1, manager);
+            MethodExecutionLogger.LogExecutionTime(task2, manager);
+            MethodExecutionLogger.LogExecutionTime(normalTask, manager);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
